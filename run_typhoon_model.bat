fc417e70715fc8ea19681b6aa1bd366652a98c3d
aklilu <akliludin@gmail.com>
2019-11-18 11:01:47 +0100

    Remove old files

--- run_typhoon_model.bat
+++
@@ -1,2 +0,0 @@
-"C:/Program Files/R/R-3.6.1/bin/Rscript.exe" C:/documents/philipiness/Typhoons/model/new_model/Typhon_models_forecast.R
-
--- run_typhoon_pythoncode.bat
+++
@@ -1,2 +0,0 @@
-"C:\anaconda\envs\py27\python.exe" "C:\documents\philipiness\automation_code_alert.py"
-pause
