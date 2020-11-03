3bd37b010c55f845a926599f883c4ce6b0980e2f
AKLILU TEKLESADIK <akliludin@gmail.com>
2020-11-03 22:20:17 +0100

    update

---
+++ lib/settings.py
@@ -0,0 +1,30 @@
+eMAIL_LIST = ['akliludin@gmail.com']
+cC_LIST = ['akliludin@gmail.com']
+eMAIL_LIST_ERROR = ['akliludin@gmail.com','ateklesadik@redcross.nl']
+fTP_LOGIN="anonymous"
+fTP_PASSWORD="anonymous"
+uCL_USERNAME="RodeKruis"
+uCL_PASSWORD="TestRK1"
+eMAIL_LOGIN="partyphoon"
+eMAIL_FROM="partyphoon@gmail.com"
+eMAIL_PASSWORD="510typhoonModel"
+sMTP_SERVER='smtp.gmail.com:587'
+
+SMTP_SERVER = 'smtp.office365.com:587'
+EMAIL_FROM = 'support@510.global'
+EMAIL_LOGIN = 'sa_typhoon@redcross.nl'
+EMAIL_PASSWORD = 'jJDVBYVHyT3czCv'
+
+EMAIL_LIST = ['akliludin@gmail.com',
+            'fbf.techadvisor@grc-philippines.org',
+            'leonardo.ebajo@redcross.org.ph',
+            'ana.mariquina@redcross.org.ph',
+            'c.marinas@redcross.org.ph',
+            'fbf1@grc-philippines.org',
+            'jannisvisser@redcross.nl']
+            
+EMAIL_LIST_ERROR = ['jannisvisser@redcross.nl',
+     'akliludin@gmail.com',
+     'ateklesadik@redcross.nl']
+     
+CC_LIST = [] 
