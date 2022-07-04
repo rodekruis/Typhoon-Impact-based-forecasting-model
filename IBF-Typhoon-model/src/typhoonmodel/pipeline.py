1a5a4b53de2ef33029bd71792a6a9345882f9727
turnerm <turnermon@gmail.com>
2022-07-04 12:48:05 +0200

    Small changes

--- IBF-Typhoon-model/src/typhoonmodel/pipeline.py
+++ IBF-Typhoon-model/src/typhoonmodel/pipeline.py
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 # -*- coding: utf-8 -*-
 """
 Created on Sat Oct 31 16:01:00 2020
@@ -19,9 +17,11 @@ decoder = Decoder()
 initialize.setup_logger()
 logger = logging.getLogger(__name__)
 
+
 @click.command()
 @click.option('--path', default='./', help='main directory')
-@click.option('--remote_directory', default=None, help='remote directory for ECMWF forecast data') #'20210421120000'
+@click.option('--remote_directory', default=None,
+              help='remote directory for ECMWF forecast data, format: YYYYMMDDhhmmss')
 @click.option('--typhoonname', default=None, help='name for active typhoon')
 @click.option('--no-azure', is_flag=True, help="Don't push to Azure lake")
 @click.option('--debug', is_flag=True, help='setting for DEBUG option')
@@ -30,21 +30,17 @@ def main(path, remote_directory, typhoonname, no_azure, debug):
     start_time = datetime.now()
     print('---------------------AUTOMATION SCRIPT STARTED---------------------------------')
     print(str(start_time))
-    #%% check for active typhoons
     print('---------------------check for active typhoons---------------------------------')
     print(str(start_time))
     remote_dir = remote_directory
-    main_path=path
     if debug:
         typhoonname = 'MEGI'
         remote_dir = '20220412000000'
-        logger.info(f"DEBUGGING piepline for typhoon{typhoonname}")
-    Forecast(main_path,remote_dir,typhoonname, countryCodeISO3='PHP', admin_level=3, no_azure=no_azure)
+        logger.info(f"DEBUGGING piepline for typhoon {typhoonname}")
+    Forecast(path, remote_dir, typhoonname, countryCodeISO3='PHP', admin_level=3, no_azure=no_azure)
     print('---------------------AUTOMATION SCRIPT FINISHED---------------------------------')
     print(str(datetime.now()))
 
 
-#%%#Download rainfall (old pipeline)
-#automation_sript(path)
 if __name__ == "__main__":
     main()
